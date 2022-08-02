9703d0eeaa57bb42ab54cff6dc2a71bed8e292e4
cafin <Daudimam123@gmail.com>
2022-08-02 20:40:20 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis error cum exercitationem obcaecati libero amet mollitia, eius laborum, delectus dolorum adipisci at ipsum unde! Deleniti pariatur consequatur tenetur reprehenderit necessitatibus?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
